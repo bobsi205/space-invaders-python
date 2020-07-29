fd6ced5d774ff051ace4e0131f06a5132fbc0adf
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-07-29 12:35:11 +0300

    moved sound to own class

--- classes/Sound.py
+++ classes/Sound.py
@@ -8,13 +8,15 @@ PLAYER_DEATH_SOUND = pygame.mixer.Sound(
     os.path.join("assets\\sound", "player_death.ogg"))
 HIT_SOUND = pygame.mixer.Sound(os.path.join("assets\\sound", "hit.ogg"))
 
-# enemy 1
+# enemys
 HIT_SOUND = pygame.mixer.Sound(os.path.join("assets\\sound", "hit.ogg"))
 ENEMY_DEATH_SOUND = pygame.mixer.Sound(
     os.path.join("assets\\sound", "enemy_death.ogg"))
 
+# weapon sound
+LASER_SOUND = pygame.mixer.Sound(os.path.join("assets\\sound", "laser.ogg"))
 
-# enemy 2
-
-
-# enemy 3
+# music
+pygame.mixer.music.load(os.path.join("assets\\sound", "music.ogg"))
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play()
